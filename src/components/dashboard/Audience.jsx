9f1fba0233899210fdff1a1913ba948e297472fa
HumayunZahoor <humayuncs009@gmail.com>
2024-09-29 14:28:11 +0500

    update

--- src/components/dashboard/Audience.jsx
+++ src/components/dashboard/Audience.jsx
@@ -42,9 +42,9 @@ const Audience = ({ onClose, onNext }) => {
                 <h4 className="text-custom-orange text-sm font-bold text-left mb-4">Audience of your book</h4>
                 
                 {/* Input for adding audiences */}
-                <div className="w-full flex flex-wrap items-center bg-[#292C30] border border-[#575757] rounded-xl p-2 mb-6">
+                <div className="lg:w-full w-[95%] flex flex-wrap items-center bg-[#292C30] border border-[#575757] rounded-xl p-2 mb-6">
                     {audiences.map((audience, index) => (
-                        <div key={index} className="flex items-center bg-custom-orange text-white rounded-full px-3 py-1 m-1">
+                        <div key={index} className="flex items-center lg:w-full w-[95%] bg-custom-orange text-white rounded-full px-3 py-1 m-1">
                             <span>{audience}</span>
                             <button
                                 className="ml-2 text-white"
